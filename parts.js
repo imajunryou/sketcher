b26170775fd6bf5b6b19b56d1739ddd70690a287
Chris Shepard <imajunryou@gmail.com>
2016-08-15 22:31:57 -0600

    Possible fix for grid generation, and user interaction bugs

--- parts.js
+++ parts.js
@@ -1,36 +1,34 @@
 //Javascript bits of an etch-a-sketch thing, based on the Odin Project's jQuery project.
 //Relies, as you might expect, on jQuery
 
-function clearGrid() {
-    $('.row').remove();
-}
-
-function generateGrid(squares) {
-    for(var i=0; i<squares; i++){
-        var row = $('<div class="row" id="row' + i + '"></div>');
-        $('.grid-wrapper').append(row);
-        for(var j = 0; j<squares; j++){
-            var square = $('<div class="square" id="sq' + (i*squares+j) + '"></div>');
-            $('#row'+i).append(square);
-        }
-    }
-}
-
 $(document).ready(function() {
-    clearGrid();
-    generateGrid(16);
 
-    $('#clear-grid-btn').on('click', function() {
-        clearGrid();
-        generateGrid(16);
+    $('.new_grid').on('click', function() {
+        var squares = +prompt("What is the side-length of your grid?", "16");
+        if (isNaN(squares)) {
+            squares = 16;
+        }
+
+        $(this).closest('.interactives').find('.grid-wrapper').empty();
+
+        for (var i = 0; i < squares; i++){
+            for (var j = 0; j < squares; j++){
+                var square = $('<div class="square" id="sq' + (i*squares+j) + '"></div>');
+                $(this).closest('.interactives').find('.grid-wrapper').append(square);
+            }
+        }
+
+        $('.square').css({"width": Math.floor((100/squares)*0.8)+"vmin",
+                         "height": Math.floor((100/squares)*0.8)+"vmin"});
+
+        $('.square').on('mouseenter', function() {
+            $(this).addClass('highlight');
+        });
+
+        $('.square').on('click', function() {
+            console.log("CLICK! " + this.id);
+        });
     });
     
-    $('.square').on('mouseenter', function() {
-        console.log(this.id);
-        $(this).addClass('highlight');
-    });
 
-    $('.square').on('click', function() {
-        console.log("CLICK! " + this.id);
-    });
 });
