8c0aa44781eba88d7df86a515421b49f9f24173e
Chris Shepard <imajunryou@gmail.com>
2016-08-16 00:36:05 -0600

    Additional work on grid layout for different sizes; still incomplete

--- parts.js
+++ parts.js
@@ -12,14 +12,21 @@ $(document).ready(function() {
         $(this).closest('.interactives').find('.grid-wrapper').empty();
 
         for (var i = 0; i < squares; i++){
+            var row = $('<div class="row" id="row' + i + '"></div>');
+            $(this).closest('.interactives').find('.grid-wrapper').append(row);
             for (var j = 0; j < squares; j++){
                 var square = $('<div class="square" id="sq' + (i*squares+j) + '"></div>');
-                $(this).closest('.interactives').find('.grid-wrapper').append(square);
+                $(this).closest('.interactives').find('#row'+i).append(square);
             }
         }
 
-        $('.square').css({"width": Math.floor((100/squares)*0.8)+"vmin",
-                         "height": Math.floor((100/squares)*0.8)+"vmin"});
+        var sizes = [960];
+        var square_size = Math.floor((sizes[0]/squares));
+        var grid_size = sizes[0];
+        $('.square').css({"width": square_size+"px",
+                         "height": square_size+"px"});
+        $('.grid-wrapper').css({"width": grid_size+"px",
+                                "height": grid_size+"px"});
 
         $('.square').on('mouseenter', function() {
             $(this).addClass('highlight');
